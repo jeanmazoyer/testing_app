1322bcb68628dbdff9097277dce2161a5fdfbc7b
Jean Mazoyer <jeanmazoyer@gmail.com>
2013-12-07 17:19:18 +0100

    Add user model with Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module TestingApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    #Require for Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TestingApp::Application.routes.draw do
+  devise_for :users
+
   root :to => 'pages#home'
   get 'about' => 'pages#about'
 
