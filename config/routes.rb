1d77a029ccc43a3d1e41d58a1791098b8bb89013
Jean Mazoyer <jeanmazoyer@gmail.com>
2013-12-10 00:00:47 +0100

    Change to pins index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ TestingApp::Application.routes.draw do
 
   devise_for :users
 
-  root :to => 'pages#home'
+  root :to => 'pins#index'
   get 'about' => 'pages#about'
 
   # The priority is based upon order of creation:
